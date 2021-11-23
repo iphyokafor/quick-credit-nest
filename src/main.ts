1d2d252354adda01bad8c449d25b35b01244e746
Okafor Ifeoma Sandra <sandraifeoma22@gmail.com>
2021-11-23 14:26:54 +0100

    Setup database configuration for postgres

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+dotenv.config();
+const port = process.env.PORT || 8000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+  await app.listen(port);
+
+  Logger.log(
+    `Server running on http://localhost:${port} in ${process.env.NODE_ENV} mode \nPress CTRL-C to stop`,
+    'Bootstrap',
+  );
 }
 bootstrap();
